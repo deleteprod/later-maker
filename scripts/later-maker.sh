57ff2d890c8335b64c392cb0b17dd78d68001307
Delete Prod <martynjamesoconnor@hotmail.co.uk>
2021-09-25 00:02:57 +0100

    Finished initial pass. Should be considered first working draft now.

--- scripts/later-maker.sh
+++ scripts/later-maker.sh
@@ -5,6 +5,23 @@ download_epoch=$(date +%s)
 url="https://mask-api.icloud.com/egress-ip-ranges.csv"
 lookup_index="private_relay"
 
+# Set local Splunk instance type - valid options are "HF" and "UF"
+splunk_instance_type="UF"
+
+# Create path for later call to Splunk from the instance type info
+if [ "$splunk_instance_type" = "UF" ]; 
+then
+  splunk_path_segment="splunkuniversalforwarder"
+elif [ "$splunk_instance_type" = "HF" ];
+then
+  splunk_path_segment="splunk"
+else
+  echo "Something weird is going on. Have you specified a forwarder type?"
+fi
+
+echo "Splunk path segment:"
+echo $splunk_path_segment
+
 # Does the lookup directory exist?
 lookups_dir="$(pwd)/lookups"
 if test -d "$lookups_dir";
@@ -51,7 +68,13 @@ echo
 # Do the comparison
 if cmp -b -s $lookups_dir/$most_recent $lookups_dir/$previous;
 then
-  # Files match - do nothing
+  # Files match - cull previous but do nothing with Splunk
+  echo "Not sending any updates to Splunk. No new data from Apple."
+  previous_lookup_file=${previous::-7}
+  rm $lookups_dir/$previous $lookups_dir/$previous_lookup_file
 else 
-  # Files don't match, do something
-
+  # Files don't match, send the new data to Splunk Cloud via local UF oneshot
+  echo "New data found. Sending to Splunk Cloud."
+  /opt/$splunk_path_segment/bin/splunk add oneshot $lookups_dir/$most_recent -index $lookup_index -sourcetype csv
+  rm $lookups_dir/$previous $lookups_dir/$previous_lookup_file
+fi
